5135c2d86f3a6f1cc174b52ff05ea3660e11dcdb
adnansabbir <adnan.sabbir2012@hmail.com>
2019-07-28 06:05:17 +0600

    Added single listing page, and property realtor

--- listings/views.py
+++ listings/views.py
@@ -1,13 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from listings.models import Listing
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 
 
 def index(request):
-    return render(request, 'listings/listings.html')
+    listings = Listing.objects.order_by('list_date').filter(is_published=True)
+
+    paginator = Paginator(listings, 6)
+    page = request.GET.get('page')
+    paged_listings = paginator.get_page(page)
+
+    context = {
+        'listings': paged_listings,
+    }
+    return render(request, 'listings/listings.html', context)
 
 
 def listing(request, listing_id):
-    return render(request, 'listings/listing.html')
+    listing = get_object_or_404(Listing, pk=listing_id)
+
+    context = {
+        'listing': listing,
+    }
+    return render(request, 'listings/listing.html', context)
 
 
 def search(request):
